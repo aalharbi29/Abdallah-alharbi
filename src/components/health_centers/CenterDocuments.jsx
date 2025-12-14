563fa7aa994807eaf0e6038617892ce07c26c7f2
A-A <r3eno141666@gmail.com>
2025-12-14 18:37:24 +0000

    File changes

--- src/components/health_centers/CenterDocuments.jsx
+++ src/components/health_centers/CenterDocuments.jsx
@@ -6,6 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import InputWithVoice from '@/components/ui/InputWithVoice';
+import { cn } from '@/lib/utils';
 import {
   Select,
   SelectContent,
